012575191e9514eda6aff4324dcd31759c9aa8f3
unknown <aakrutisaraf29@gmail.com>
2022-10-10 15:46:28 +0530

    user get incomplete

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,49 @@
+import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:learning/home.dart';
 import 'package:learning/profile.dart';
+import 'package:learning/userProfile.dart';
 
 import 'amplifyconfiguration.dart';
+import 'models/ModelProvider.dart';
 
 void main() {
-  runApp( const MyDemoApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+  static const appTitle = 'Learning with Dr.SK Srivastava';
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  late AmplifyDataStore datastorePlugin;
+
   @override
   void initState() {
     super.initState();
     _configureAmplify();
   }
-
+//final AmplifyDataStore _amplifyDataStore = AmplifyDataStore(model : )
   void _configureAmplify() async {
     try {
       await Amplify.addPlugin(AmplifyAuthCognito());
+      await Amplify.addPlugin(AmplifyAPI(        modelProvider: ModelProvider.instance,
+      ));
+      datastorePlugin = AmplifyDataStore(
+        modelProvider: ModelProvider.instance,
+        errorHandler: ((error) =>
+        {print("Custom ErrorHandler received: " + error.toString())}),
+      );
+      await Amplify.addPlugin(datastorePlugin);
+
       await Amplify.configure(amplifyconfig);
       print('Successfully configured');
     } on Exception catch (e) {
@@ -40,13 +56,97 @@ class _MyAppState extends State<MyApp> {
     return Authenticator(
       child: MaterialApp(
         builder: Authenticator.builder(),
-
-        home: const Scaffold(
-          body: Center(
-            child: Text('You are logged in!'),
-          ),
-        ),
+        home: const MyMaterialApp()
       ),
+    );
+  }
+}
+
+class MyMaterialApp extends StatelessWidget {
+  const MyMaterialApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(MyApp.appTitle)),
+        body: const Center(
+          child: Text('Home Page!'),
+        ),
+        drawer:Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const UserAccountsDrawerHeader(
+                // decoration: BoxDecoration(color: const Color(0xff764abc)),
+                accountName: Text(
+                  "Aakruti Saraf",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                accountEmail: Text(
+                  "aakruti@aavrti.com",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                currentAccountPicture: FlutterLogo(),
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.home,
+                ),
+                title: const Text ('Home'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.person,
+                ),
+                title: const Text('Profile'),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyUserProfileApp()));
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.list,
+                ),
+                title: const Text('Categories'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+
+              ListTile(
+                leading: const Icon(
+                  Icons.settings,
+                ),
+                title: const Text('Settings'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+
+
+              const AboutListTile( // <-- SEE HERE
+                icon: Icon(
+                  Icons.info,
+                ),
+                child: Text('App Info'),
+                applicationIcon: Icon(
+                  Icons.local_play,
+                ),
+                applicationName: 'Learing with Dr.SK Srivastava',
+                applicationVersion: '1.0.0',
+              ),
+            ],
+          ),
+        )
     );
   }
 }
--- lib/userProfile.dart
+++ lib/userProfile.dart
@@ -1,4 +1,4 @@
-// import 'package:amplify_api/model_queries.dart';
+import 'package:amplify_api/model_queries.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:learning/home.dart';
@@ -6,7 +6,10 @@ import 'dart:io';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:learning/models/Users.dart';
 import 'package:learning/profile.dart';
+
 class MyUserProfileApp extends StatefulWidget {
+  const MyUserProfileApp({super.key});
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -43,23 +46,51 @@ class _MyAppState extends State<MyUserProfileApp> {
               },
             ),
             title: const Center(child: Text('User Profile ')),
+
+
+          ),
+          body: FutureBuilder<Users?>(
+            future: queryItem(),
+            builder: (context, snapshot) {
+              if(snapshot.hasData) {
+                return Container();
+
+              } else {
+                return const CircularProgressIndicator();
+
+              }},
           ),
         )
     );
   }
-  // Future<Users?> queryItem(Users queriedUsers) async {
-  //   try {
-  //     final request = ModelQueries.get(Users.classType, queriedUsers.id);
-  //     final response = await Amplify.API.query(request: request).response;
-  //     final users = response.data;
-  //     if (users == null) {
-  //       print('errors: ${response.errors}');
-  //     }
-  //     return users;
-  //   }
-  //   on ApiException catch (e) {
-  //     print('Query failed: $e');
-  //     return null;
-  //   }
-  // }
+
+  Future<AuthUser> getCurrentUser() async {
+    final user = await Amplify.Auth.getCurrentUser();
+    return user;
+  }
+
+  Future<Users?> queryItem() async {
+    try {
+      final user = await getCurrentUser();
+      print("Cognito USer Info$user");
+      print("Cognito USer Info${user.userId}");
+      print("Cognito USer Info${user.username}");
+
+      final request = ModelQueries.get(Users.classType, user.userId);
+      final response = await Amplify.API.query(request: request).response;
+      final users = response.data;
+      print("User Info$response");
+      if (users == null) {
+        print('errors: ${response.errors}');
+      }
+      return users;
+    }
+    on ApiException catch (e) {
+      print('Query failed: $e');
+      return null;
+    }on  Exception catch (e) {
+    print('Query failed: $e');
+    return null;
+    }
+  }
 }
